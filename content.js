4489483f8f72186655846542abb4ecd1f95507c0
OKinane <15254824+OKinane@users.noreply.github.com>
2023-07-11 23:49:49 +0100

    replace all spotify embedded players by the some youtube video

--- content.js
+++ content.js
@@ -7,6 +7,10 @@ window.addEventListener("load", function() {
     // You can use DOM manipulation techniques to identify and modify the page content
     // For example, you can search for Spotify iframe elements and replace them with YouTube iframe elements
     // Remember to respect the terms of service of the services you are interacting with
+    for (const ifr of document.querySelectorAll("iframe[src*='spotify.com/embed']")) {
+        const youtubeVideoId = "NF-nlerWuc4";
+        ifr.outerHTML = `<iframe src="https://www.youtube.com/embed/${youtubeVideoId}" frameborder="0"></iframe>`;
+    }
   }
 
   // Send a message to the background script when the page content changes (optional)
