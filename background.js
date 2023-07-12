5d8f6f8f5c2457348f5739888b5d5bd4260ac8cb
OKinane <15254824+OKinane@users.noreply.github.com>
2023-07-12 01:41:12 +0100

    simpler approach to replace all spotify embedded players

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.message === "replaceSpotifyPlayer") {
-      replaceSpotifyPlayerWithYouTubePlayer();
-    }
-  });
-
-  function replaceSpotifyPlayerWithYouTubePlayer() {
-    // Logic to replace Spotify player with YouTube player goes here
-  }
--- content.js
+++ content.js
@@ -1,21 +1,10 @@
 window.addEventListener("load", function() {
-    replaceSpotifyPlayerWithYouTubePlayer();
-  });
-
-  function replaceSpotifyPlayerWithYouTubePlayer() {
-    // Logic to find and replace Spotify player with YouTube player goes here
-    // You can use DOM manipulation techniques to identify and modify the page content
-    // For example, you can search for Spotify iframe elements and replace them with YouTube iframe elements
-    // Remember to respect the terms of service of the services you are interacting with
-    for (const ifr of document.querySelectorAll("iframe[src*='spotify.com/embed']")) {
-        const youtubeVideoId = "NF-nlerWuc4";
-        ifr.outerHTML = `<iframe src="https://www.youtube.com/embed/${youtubeVideoId}" frameborder="0"></iframe>`;
+    if (this.document.location.href.includes(".spotify.com/embed")) {
+        replaceSpotifyPlayer();
     }
-  }
-
-  // Send a message to the background script when the page content changes (optional)
-  var observer = new MutationObserver(function(mutations) {
-    replaceSpotifyPlayerWithYouTubePlayer();
   });
 
-  observer.observe(document.body, { childList: true, subtree: true });
+function replaceSpotifyPlayer() {
+    const youtubeVideoId = "NF-nlerWuc4";
+    document.location.href = `https://www.youtube-nocookie.com/embed/${youtubeVideoId}`;
+}
