b17c32c750a90466f983b73aa46223b24f6c814f
OKinane <15254824+OKinane@users.noreply.github.com>
2023-07-12 01:42:22 +0100

    parse track info (band + title)

--- content.js
+++ content.js
@@ -5,6 +5,11 @@ window.addEventListener("load", function() {
   });
 
 function replaceSpotifyPlayer() {
+    const text = document.querySelector("body").innerText;
+    const z = text.split('\n');
+    console.log("****** url:", document.location.href);
+    console.log("****** band:", z[1]);
+    console.log("****** song:", z[0]);
     const youtubeVideoId = "NF-nlerWuc4";
     document.location.href = `https://www.youtube-nocookie.com/embed/${youtubeVideoId}`;
 }
