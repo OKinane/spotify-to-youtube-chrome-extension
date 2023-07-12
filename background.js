97577482ed89c004f012a562c5e9d4968840dae1
OKinane <15254824+OKinane@users.noreply.github.com>
2023-07-12 11:03:46 +0100

    formatting

--- background.js
+++ background.js
@@ -1,15 +1,20 @@
-  chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.message === 'performYouTubeSearch') {
-      const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(request.query)}`;
+chrome.runtime.onMessage.addListener(function (request, sender) {
+    if (request.message === "performYouTubeSearch") {
+        const url = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+            request.query
+        )}`;
 
-      fetch(url)
-        .then(response => response.text())
-        .then(htmlContent => {
-            const firstVideoId = htmlContent.match('"videoId":"(.+?)"')[1];
-            chrome.tabs.sendMessage(sender.tab.id, { message: 'youtubeSearchResults', firstVideoId });
-        })
-        .catch(error => {
-          console.error('Error performing YouTube search:', error);
-        });
+        fetch(url)
+            .then((response) => response.text())
+            .then((htmlContent) => {
+                const firstVideoId = htmlContent.match('"videoId":"(.+?)"')[1];
+                chrome.tabs.sendMessage(sender.tab.id, {
+                    message: "youtubeSearchResults",
+                    firstVideoId,
+                });
+            })
+            .catch((error) => {
+                console.error("Error performing YouTube search:", error);
+            });
     }
-  });
+});
--- content.js
+++ content.js
@@ -1,17 +1,20 @@
 if (this.document.location.href.includes(".spotify.com/embed")) {
     // Listen for messages from the background script
-    chrome.runtime.onMessage.addListener(function(request) {
-      if (request.message === 'youtubeSearchResults') {
-        const youtubeVideoId = request.firstVideoId;
-        if (youtubeVideoId) {
-            document.location.href = `https://www.youtube-nocookie.com/embed/${youtubeVideoId}`;
+    chrome.runtime.onMessage.addListener(function (request) {
+        if (request.message === "youtubeSearchResults") {
+            const youtubeVideoId = request.firstVideoId;
+            if (youtubeVideoId) {
+                document.location.href = `https://www.youtube-nocookie.com/embed/${youtubeVideoId}`;
+            }
         }
-      }
     });
 
-    window.addEventListener("load", function() {
+    window.addEventListener("load", function () {
         const text = document.querySelector("body").innerText;
-        const [title, band, ..._] = text.split('\n');
-        chrome.runtime.sendMessage({ message: 'performYouTubeSearch', query: `${band} ${title}` });
-  });
+        const [title, band, ..._] = text.split("\n");
+        chrome.runtime.sendMessage({
+            message: "performYouTubeSearch",
+            query: `${band} ${title}`,
+        });
+    });
 }
